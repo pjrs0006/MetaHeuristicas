35cccec78b356798cfa5fb44478ed1c12960419b
pjrs0006 <pjrs0006@red.ujaen.es>
2024-09-15 19:21:57 +0200

    Comiezo metodo metodo rellenamatriz

--- main.py
+++ main.py
@@ -1,24 +1,2 @@
-# This is a sample Python script.
-
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Fernando')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
-#A partir de aqui comenzara el codigo para la lectura de ficheros.
-
-#Crearemos una lista donde tendremos la ubicacion de todos los archivos para cargarlos cuando los seleccionemos
-
-#Seleccionamos el fichero que queremos leer
-
-#Volcamos los datos del fichero a la clase
